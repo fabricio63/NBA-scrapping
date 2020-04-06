8d636968449554fbcd7cc94e39bcd3b04bc29143
StevenWilson121 <49218333+StevenWilson121@users.noreply.github.com>
2020-04-06 16:08:45 -0600

    Added Flask API
    Running on http://127.0.0.1:5000/

--- nbaAPP.py
+++ nbaAPP.py
@@ -1,1 +1,23 @@
+from __future__ import print_function
+import yaml
 
+from flask import Flask, jsonify, render_template, request
+import os, optparse
+
+app = Flask(__name__)
+
+developer = os.getenv("DEVELOPER", "Fabricio Juarez & Steven Wilson")
+environment =  os.getenv("ENVIRONMENT", "Development")
+
+@app.route("/")
+def home():
+    foo = "bar"
+    return render_template("home.html", mivariable = foo, developer = developer)
+
+if __name__ == "__main__":
+
+    debug = False
+
+    if environment == "Development" or environment == "local":
+        debug = True
+    app.run(host = "127.0.0.1", debug = debug)
