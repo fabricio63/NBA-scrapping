c0fd56a0d2b1ddb57875819a3270e4fb6a17a8b6
StevenWilson121 <49218333+StevenWilson121@users.noreply.github.com>
2020-04-07 16:57:23 -0600

    updated scrapper only headers

--- scrapper.py
+++ scrapper.py
@@ -10,15 +10,23 @@ url = "https://www.basketball-reference.com/leagues/NBA_{}_standings.html".forma
 html = urlopen(url)
 soup = BeautifulSoup(html, features="lxml")
 
-
+# Scraps eastern conference headers 
 easternTable = soup.findAll('table', attrs={"id":"confs_standings_E"})
 headers = []
-
 for tablerows in easternTable:
     tr = tablerows.find_all('tr', limit=1)
     for text in tr:
         headers = text.getText().strip().split("\n")
-        
 
 print(headers)
+
+# # avoid the first header row
+# for 
+# rows = soup.findAll('tr')[1:]
+# print(rows)
+# # teamStats = [[td.getText() for td in rows[i].findAll('td')]
+# #             for i in range(len(rows))]
+
+
+
 
