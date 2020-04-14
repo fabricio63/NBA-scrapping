425e3b48b8720a7fc197bd9326e2a1ac740dce6c
fabricio_ufm <fabriciojuarez@ufm.edu>
2020-04-14 13:32:27 -0600

    Scrapper found western conference stats

--- srapping.py
+++ srapping.py
@@ -19,7 +19,7 @@ for tablerows in easternTable:
     for text in tr:
         headers = text.getText().strip().split("\n")
 
-teams = []
+eteams = []
 for tbody in easternTable:
     tableBody = tablerows.find_all('tbody')
     for item in tableBody:
@@ -32,10 +32,31 @@ for tbody in easternTable:
         eopp_pts_pg = item.find_all('td', attrs = {"data-stat" :"opp_pts_per_g"})
         econt = 0
         for name in href:
-            teams.append({name.text:"wins: "+ ewins[econt].text + " - " +"losses: "+ loss[cont].text})
-            cont = cont +1
+            eteams.append({name.text:"wins: "+ ewins[econt].text + " - " +"losses: "+ eloss[econt].text})
+            econt = econt +1
 
+WesternTable = soup.findAll('table', attrs={"id":"confs_standings_W"})
+wheaders = []
+for tablerows in WesternTable:
+    tr = tablerows.find_all('tr', limit=1)
+    for text in tr:
+        wheaders = text.getText().strip().split("\n")
 
+wteams = []
+for tbody in WesternTable:
+    tableBody = tablerows.find_all('tbody')
+    for item in tableBody:
+        href = item.find_all("a", href= True)
+        wwins = item.find_all('td', attrs = {"data-stat" :"wins"})
+        wloss = item.find_all('td', attrs = {"data-stat" :"losses"})
+        wwin_loss = item.find_all('td', attrs = {"data-stat" :"win_loss_pct"})
+        wgames_back = item.find_all('td', attrs = {"data-stat" :"gb"})
+        wpts_p_g = item.find_all('td', attrs = {"data-stat" :"pts_per_g"})
+        wopp_pts_pg = item.find_all('td', attrs = {"data-stat" :"opp_pts_per_g"})
+        wcont = 0
+        for name in href:
+            wteams.append({name.text:"wins: "+ wwins[wcont].text + " - " +"losses: "+ wloss[wcont].text})
+            wcont = wcont +1
 
-for i in teams:
+for i in wteams:
     print(i)
