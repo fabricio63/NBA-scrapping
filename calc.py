35928d4c45124d07ae084182326d5574e4b46eb3
fabricio_ufm <fabriciojuarez@ufm.edu>
2020-05-17 14:36:42 -0600

    changes to calc and adds csv to jmeter

--- calc.py
+++ calc.py
@@ -5,19 +5,12 @@ def calculadora (operacion,lista):
         avg = sum(lista)/len(lista)
         return str(avg)
     if operacion == "max":
-        return max(lista)
+        return str(max(lista))
     if operacion == "min":
-        return min(lista)
+        return str(min(lista))
     if operacion == "stdev":
         avg = sum(lista)/len(lista)
         return str(statistics.stdev(lista,avg))
-    if operacion == "median":
-        ln = len(lista)
-        sorted_list= sorted(lista)
-        middle = (ln / 2) + 0.5
-        return str(sorted_list[middle])
-    if operacion == "mode":
-        return str(statistics.mode(lista))
     if operacion == "range":
         return str(max(lista)-min(lista))
     else:
