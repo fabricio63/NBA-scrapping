1a356cc5b62c47c36c47d6149eb489a637e65562
fabricio_ufm <fabriciojuarez@ufm.edu>
2020-05-17 07:08:37 -0600

    adds stats route adds calc adds variable season adds query parameters

---
+++ calc.py
@@ -0,0 +1,24 @@
+import statistics
+
+def calculadora (operacion,lista):
+    if operacion == "average":
+        avg = sum(lista)/len(lista)
+        return str(avg)
+    if operacion == "max":
+        return max(lista)
+    if operacion == "min":
+        return min(lista)
+    if operacion == "stdev":
+        avg = sum(lista)/len(lista)
+        return str(statistics.stdev(lista,avg))
+    if operacion == "median":
+        ln = len(lista)
+        sorted_list= sorted(lista)
+        middle = (ln / 2) + 0.5
+        return str(sorted_list[middle])
+    if operacion == "mode":
+        return str(statistics.mode(lista))
+    if operacion == "range":
+        return str(max(lista)-min(lista))
+    else:
+        return "our calc doesnt support your request"
